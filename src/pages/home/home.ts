f44715670e1500381cf12cd16bee7eb433892827
Juan Monsalve <juanmonsalveh@gmail.com>
2017-07-31 00:14:38 -0500

    Navigation example to "notification page" implemented 	-import and declaration (app.module.ts) 	-navigation button (home.html) 	-import and declaration of a usable variable  (home.ts)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NotificationPage} from '../notification/notification';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	notificationPage = NotificationPage;
 
   constructor(public navCtrl: NavController) {
 

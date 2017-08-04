096663828ceb7e863631220f0d6708609a468f55
Juan Monsalve <juanmonsalveh@gmail.com>
2017-08-04 17:11:59 -0500

    notification configurations has been ended, lets´s start coding (added moment library to work  with dates)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { LocalNotifications } from '@ionic-native/local-notifications';//lib
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import {NotificationPage} from '../pages/notification/notification';
+import {NotificationPage} from '../pages/notification/notification';//pag
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    NotificationPage
+    NotificationPage//pag
   ],
   imports: [
     BrowserModule,
@@ -25,6 +26,7 @@ import {NotificationPage} from '../pages/notification/notification';
     NotificationPage
   ],
   providers: [
+    LocalNotifications, //lib
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
